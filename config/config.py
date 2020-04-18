19ef26d3c7f372e1b556ba54567328acdce64558
Ukor Jidechi E <ukorjidechi@gmail.com>
2020-04-18 22:13:44 +0100

    Added dependency and updated config files

--- config/config.py
+++ config/config.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 config_obj = {
-    "env": "dev", # "production",
+    # production || dev
+    "env": "production",
     "es_prod": "https://search-hk-demo-domain-cmwk32rmeh6dmoykeptm245iqa.eu-central-1.es.amazonaws.com",
     "dev_destination": f"{str(Path.home())}/programming_projects/knockhome/map-test-backend/mapTestBackend/polygons",
     "demo_destination": f"/var/www/hk_polygons",
