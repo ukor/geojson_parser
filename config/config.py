f2d622ec50f822136fa2bd59653581d1c5ee92ea
Ukor Jidechi E <ukorjidechi@gmail.com>
2020-05-02 18:32:33 +0100

    feat: updated configuration file

--- config/config.py
+++ config/config.py
@@ -1,9 +1,20 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-config_obj = {
-    # production || dev
-    "env": "production",
-    "es_prod": "https://search-hk-demo-domain-cmwk32rmeh6dmoykeptm245iqa.eu-central-1.es.amazonaws.com",
-    "dev_destination": f"{str(Path.home())}/programming_projects/knockhome/map-test-backend/mapTestBackend/polygons",
-    "demo_destination": f"/var/www/hk_polygons",
-}
+load_dotenv()
+
+def setPolygonDestination(env: str) -> str:
+    if env == "dev":
+        return f"{str(Path.home())}/programming_projects/knockhome/map-test-backend/mapTestBackend/polygons",
+    return getenv("POLYGON_DESTINATION")
+
+ENV = getenv("ENV")
+POLYGON_DESTINATION = setPolygonDestination(ENV)
+ES_ENDPOINT = getenv("AWS_ES_ENDPOINT")
+ES_INDEX_NAME = getenv("ES_INDEX_NAME")
+
+AWS_USER = getenv("AWS_USER")
+AWS_ACCESS_KEY_ID = getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = getenv("AWS_REGION")
