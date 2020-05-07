9792b31c7c019e305f0ee460cab181603c05d417
Ukor Jidechi E <ukorjidechi@gmail.com>
2020-05-07 23:01:41 +0100

    fix: postcode area index

--- delete.py
+++ delete.py
@@ -4,12 +4,13 @@ from es.es import es_client
 from es.es_config import ConfigElasticSearch
 from es_instance import es_instance
 
-scopes = ["place", "admin", "london_borough", "area", "postcode_area", "ward", "parish"]
+# scopes = ["place", "admin", "london_borough", "area", "postcode_area", "ward", "parish"]
+scopes = ["postcode_area"]
 
 for scope in scopes:
     _es_instance = es_instance()
 
-    _es_client = es_client(es_instance=_es_instance)
+    _es_client = es_client(es_instance=_es_instance, es_index="hk_places")
 
     _es_client.delete_by_query(scope)
     time.sleep(1.30)
